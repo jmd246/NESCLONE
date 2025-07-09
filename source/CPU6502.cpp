fb6203f49be72b41f031c6f9424113b91ebdbcac
jmd246 <jdoucette69.jd@gmail.com>
2025-07-09 16:34:47 -0400

    Added more instructions and added addressing mode ACC added functionallity to handle fetches with accumulator addressing mode

--- source/CPU6502.cpp
+++ source/CPU6502.cpp
@@ -10,10 +10,10 @@ CPU6502::CPU6502() {
 		{ "BPL", &a::BPL, &a::REL, 2 },{ "ORA", &a::ORA, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ZPX, 4 },{ "ASL", &a::ASL, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLC", &a::CLC, &a::IMP, 2 },{ "ORA", &a::ORA, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ABX, 4 },{ "ASL", &a::ASL, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
 		{ "JSR", &a::JSR, &a::ABS, 6 },{ "AND", &a::AND, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "BIT", &a::BIT, &a::ZP0, 3 },{ "AND", &a::AND, &a::ZP0, 3 },{ "ROL", &a::ROL, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PLP", &a::PLP, &a::IMP, 4 },{ "AND", &a::AND, &a::IMM, 2 },{ "ROL", &a::ROL, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "BIT", &a::BIT, &a::ABS, 4 },{ "AND", &a::AND, &a::ABS, 4 },{ "ROL", &a::ROL, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
 		{ "BMI", &a::BMI, &a::REL, 2 },{ "AND", &a::AND, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "AND", &a::AND, &a::ZPX, 4 },{ "ROL", &a::ROL, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SEC", &a::SEC, &a::IMP, 2 },{ "AND", &a::AND, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "AND", &a::AND, &a::ABX, 4 },{ "ROL", &a::ROL, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
-		{ "RTI", &a::RTI, &a::IMP, 6 },{ "EOR", &a::EOR, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "EOR", &a::EOR, &a::ZP0, 3 },{ "LSR", &a::LSR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PHA", &a::PHA, &a::IMP, 3 },{ "EOR", &a::EOR, &a::IMM, 2 },{ "LSR", &a::LSR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::ABS, 3 },{ "EOR", &a::EOR, &a::ABS, 4 },{ "LSR", &a::LSR, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
-		{ "BVC", &a::BVC, &a::REL, 2 },{ "EOR", &a::EOR, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ZPX, 4 },{ "LSR", &a::LSR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLI", &a::CLI, &a::IMP, 2 },{ "EOR", &a::EOR, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ABX, 4 },{ "LSR", &a::LSR, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
-		{ "RTS", &a::RTS, &a::IMP, 6 },{ "ADC", &a::ADC, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "ADC", &a::ADC, &a::ZP0, 3 },{ "ROR", &a::ROR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PLA", &a::PLA, &a::IMP, 4 },{ "ADC", &a::ADC, &a::IMM, 2 },{ "ROR", &a::ROR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::IND, 5 },{ "ADC", &a::ADC, &a::ABS, 4 },{ "ROR", &a::ROR, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
-		{ "BVS", &a::BVS, &a::REL, 2 },{ "ADC", &a::ADC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ZPX, 4 },{ "ROR", &a::ROR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SEI", &a::SEI, &a::IMP, 2 },{ "ADC", &a::ADC, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ABX, 4 },{ "ROR", &a::ROR, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+		{ "RTI", &a::RTI, &a::IMP, 6 },{ "EOR", &a::EOR, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "EOR", &a::EOR, &a::ZP0, 3 },{ "LSR", &a::LSR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PHA", &a::PHA, &a::IMP, 3 },{ "EOR", &a::EOR, &a::IMM, 2 },{ "LSR", &a::LSR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::ABS, 3 },{ "EOR", &a::EOR, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 6 },{ "LSR", &a::LSR, &a::ACC, 2 },
+		{ "BVC", &a::BVC, &a::REL, 2 },{ "EOR", &a::EOR, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ZPX, 4 },{ "LSR", &a::LSR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLI", &a::CLI, &a::IMP, 2 },{ "EOR", &a::EOR, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ABX, 4 },{ "???", &a::XXX, &a::IMP, 7 },{ "LSR", &a::LSR, &a::ZP0, 5 },
+		{ "RTS", &a::RTS, &a::IMP, 6 },{ "ADC", &a::ADC, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "ADC", &a::ADC, &a::ZP0, 3 },{ "ROR", &a::ROR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PLA", &a::PLA, &a::IMP, 4 },{ "ADC", &a::ADC, &a::IMM, 2 },{ "ROR", &a::ROR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::IND, 5 },{ "ADC", &a::ADC, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 6 },{ "LSR", &a::LSR, &a::ZPX, 6 },
+		{ "BVS", &a::BVS, &a::REL, 2 },{ "ADC", &a::ADC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ZPX, 4 },{ "ROR", &a::ROR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SEI", &a::SEI, &a::IMP, 2 },{ "ADC", &a::ADC, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ABX, 4 },{ "???", &a::XXX, &a::IMP, 7 },{ "LSR", &a::LSR, &a::ABS, 6 },
 		{ "???", &a::NOP, &a::IMP, 2 },{ "STA", &a::STA, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "STY", &a::STY, &a::ZP0, 3 },{ "STA", &a::STA, &a::ZP0, 3 },{ "STX", &a::STX, &a::ZP0, 3 },{ "???", &a::XXX, &a::IMP, 3 },{ "DEY", &a::DEY, &a::IMP, 2 },{ "???", &a::NOP, &a::IMP, 2 },{ "TXA", &a::TXA, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "STY", &a::STY, &a::ABS, 4 },{ "STA", &a::STA, &a::ABS, 4 },{ "STX", &a::STX, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 4 },
 		{ "BCC", &a::BCC, &a::REL, 2 },{ "STA", &a::STA, &a::IZY, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "STY", &a::STY, &a::ZPX, 4 },{ "STA", &a::STA, &a::ZPX, 4 },{ "STX", &a::STX, &a::ZPY, 4 },{ "???", &a::XXX, &a::IMP, 4 },{ "TYA", &a::TYA, &a::IMP, 2 },{ "STA", &a::STA, &a::ABY, 5 },{ "TXS", &a::TXS, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 5 },{ "???", &a::NOP, &a::IMP, 5 },{ "STA", &a::STA, &a::ABX, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "???", &a::XXX, &a::IMP, 5 },
 		{ "LDY", &a::LDY, &a::IMM, 2 },{ "LDA", &a::LDA, &a::IZX, 6 },{ "LDX", &a::LDX, &a::IMM, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "LDY", &a::LDY, &a::ZP0, 3 },{ "LDA", &a::LDA, &a::ZP0, 3 },{ "LDX", &a::LDX, &a::ZP0, 3 },{ "???", &a::XXX, &a::IMP, 3 },{ "TAY", &a::TAY, &a::IMP, 2 },{ "LDA", &a::LDA, &a::IMM, 2 },{ "TAX", &a::TAX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "LDY", &a::LDY, &a::ABS, 4 },{ "LDA", &a::LDA, &a::ABS, 4 },{ "LDX", &a::LDX, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 4 },
@@ -22,6 +22,7 @@ CPU6502::CPU6502() {
 		{ "BNE", &a::BNE, &a::REL, 2 },{ "CMP", &a::CMP, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "CMP", &a::CMP, &a::ZPX, 4 },{ "DEC", &a::DEC, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLD", &a::CLD, &a::IMP, 2 },{ "CMP", &a::CMP, &a::ABY, 4 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "CMP", &a::CMP, &a::ABX, 4 },{ "DEC", &a::DEC, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
 		{ "CPX", &a::CPX, &a::IMM, 2 },{ "SBC", &a::SBC, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "CPX", &a::CPX, &a::ZP0, 3 },{ "SBC", &a::SBC, &a::ZP0, 3 },{ "INC", &a::INC, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "INX", &a::INX, &a::IMP, 2 },{ "SBC", &a::SBC, &a::IMM, 2 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::SBC, &a::IMP, 2 },{ "CPX", &a::CPX, &a::ABS, 4 },{ "SBC", &a::SBC, &a::ABS, 4 },{ "INC", &a::INC, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
 		{ "BEQ", &a::BEQ, &a::REL, 2 },{ "SBC", &a::SBC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ZPX, 4 },{ "INC", &a::INC, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SED", &a::SED, &a::IMP, 2 },{ "SBC", &a::SBC, &a::ABY, 4 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ABX, 4 },{ "INC", &a::INC, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+		{ "LSR", &a::LSR, &a::ABX, 7 },{"ROR",&a::ROR,&a::ACC,2},{"ROR",&a::ROR,&a::ZP0,5} ,{"ROR",&a::ROR,&a::ZPX,6},{"ROR",&a::ROR,&a::ABS,6},{"ROR",&a::ROR,&a::ABX,7}, {"ROL",&a::ROL,&a::ACC,2},{"ROL",&a::ROL,&a::ZP0,5}, {"ROL",& a::ROL,& a::ZPX,6}
 	};
 }
 
@@ -194,12 +195,17 @@ uint8_t	CPU6502::REL() {
 	}
 	return	0;
 }
-
+uint8_t CPU6502::ACC() {
+	return 0;
+}
 //instructions
 uint8_t CPU6502::fetch() {
-	if (lookup[opcode].addrmode != IMM) {
+	if (lookup[opcode].addrmode != IMM && lookup[opcode].addrmode != ACC) {
 		//get value instruction will use from the address
 		fetched = read(addr_abs);
+	}
+	else if (lookup[opcode].addrmode == ACC) {
+		fetched = a;
 	}
 	return fetched;
 }
@@ -376,6 +382,7 @@ uint8_t CPU6502::BVS() {
    */
 uint8_t CPU6502::ADC() {
 	fetch();
+	//including carry bit allows adding pass range of 8 bits
 	uint16_t temp = (uint16_t) a + (uint16_t) getFlag(C) + (uint16_t) fetched;
 	//if temp is more than 255 set cary bit
 	setFlag(C, temp > 255);
@@ -390,9 +397,11 @@ uint8_t CPU6502::ADC() {
 }
 //A = A - M - (1-c) -> A = A - (M - (1-c)) -> A = A - M + c + 1
 uint8_t CPU6502::SBC() {
+	//occupy fetch with value at current address
 	fetch();
-	//ivert bottom 8 bits of M to -M
+	//invert bottom 8 bits of M to -M
 	uint16_t val = ((uint16_t)fetched) ^ 0x00FF;
+	//including carry bit allows subtracting pass range of 8 bits
 	uint16_t temp = (uint16_t)a + (uint16_t)getFlag(C) + val;
 	setFlag(C, temp &  0xFF00);
 	//if low byte is zero set zero flag
@@ -413,14 +422,33 @@ uint8_t CPU6502::PHA() {
 	sp--;
 	return 0;
 }
+uint8_t CPU6502::PHP() {
+	//write to the stack     (0x0100) is a hardcoded value where the memory for stack starts
+	write(0x0100 + sp, status | 0x30);  // push NV11BDIZC onto stack
+	//decrement stack
+	sp--;
+	return 0;
+}
 //pop
-uint8_t CPU6502::PHA() {
+uint8_t CPU6502::PLA() {
 	sp++;
 	//write to the stack     (0x0100) is a hardcoded value where the memory for stack starts
-	read(0x0100 + sp);  // read stack at sp
+	a = read(0x0100 + sp);  // read stack at sp
 	//set zero and negative flags
 	setFlag(N, a & 0x80);
-	setFlag(C, a == 0x00);
+	setFlag(Z, a == 0x00);
+	return 0;
+}
+//pull  processor  status
+uint8_t CPU6502::PLP() {
+	//1. increment stack pointer to get most recent byte
+	sp++;
+	//read byte from stack and load in into status
+	status = read(0x0100 + sp);
+	//clear bit B position 4 ;
+	status &= 0xEF;
+	//set bit U
+	status |= 0x20;
 	return 0;
 }
 // interuots
@@ -488,4 +516,175 @@ uint8_t CPU6502::RTI() {
 	sp++;
 	pc |= (uint16_t)read(0x0100 + sp) << 8;
 	return 0;
+}
+
+//asl brk bit dec inc inx
+
+
+uint8_t CPU6502::INC() {
+	fetch();
+	
+	fetched += 1;
+	
+	setFlag(Z, fetched == 0x00);
+	setFlag(N, fetched & 0x80);
+
+	write(addr_abs, fetched);
+
+	return 0;
+
+}
+uint8_t CPU6502::INX() {
+	x += 1;
+	setFlag(Z, x == 0x00);
+	setFlag(N, x & 0x80);
+	return 0;
+}
+uint8_t CPU6502::INY() {
+	y += 1;
+	setFlag(Z, y == 0x00);
+	setFlag(N, y & 0x80);
+	return 0;
+}
+uint8_t CPU6502::DEC() {
+	fetch();
+
+	fetched -= 1;
+
+	setFlag(Z, fetched == 0x00);
+	setFlag(N, fetched & 0x80);
+
+	write(addr_abs, fetched);
+
+	return 0;
+}
+
+uint8_t CPU6502::ROR() {
+	//carry bit is value of bit 0
+	if (lookup[opcode].addrmode == ACC) {
+		uint8_t carryIn = getFlag(C) << 7;
+		//save bit 0 into carry
+		setFlag(C, a & 0x01);
+		//carry in goes into bit 7
+		a = (a >> 1) | carryIn;
+		setFlag(Z, a == 0);
+		setFlag(N, a & 0x80);
+	}
+	else {
+		fetch();
+		uint8_t carryIn = getFlag(C) << 7;
+		setFlag(C, fetched & 0x01);
+		uint8_t result = (fetched >> 1) | carryIn;
+		setFlag(Z, result == 0);
+		setFlag(N, result & 0x80);
+		//write to memory
+		write(addr_abs, result);
+	}
+	return 0;
+}
+uint8_t CPU6502::ROL() {
+	if (lookup[opcode].addrmode == ACC) {
+		uint8_t carryIn = getFlag(C);
+		//save bit 7 into carry
+		setFlag(C, a & 0x80);
+		a = (a << 1) | carryIn;
+		setFlag(Z, a == 0);
+		setFlag(N, a & 0x80);
+	}
+	else {
+		//fetch from memory
+		fetch();
+		uint8_t carryIn = getFlag(C);
+		//save bit 7 into carry
+		setFlag(C, fetched & 0x80);
+		uint8_t result = (fetched << 1) | carryIn;
+		setFlag(Z, result == 0);
+		setFlag(N, result & 0x80);
+		write(addr_abs, result);	
+	}
+	return 0;
+}
+
+uint8_t CPU6502::LSR() {
+	//operate on either memory or accumulator
+	if (lookup[opcode].addrmode == &CPU6502::ACC) {
+		//carry bit is value of bit 0
+		setFlag(C, a & 0x01);
+		//shift right
+		a >>= 1;
+		//negative bit is set to 0
+		setFlag(N, 0);
+		setFlag(Z, a == 0x00);
+	}
+	else {
+		fetch();
+		setFlag(C, fetched & 0x01);
+		uint8_t result = fetched >> 1;
+		setFlag(Z, result == 0x00);
+		setFlag(N, 0);
+		write(addr_abs, result);
+	}
+	return 0;
+}
+uint8_t CPU6502::JMP() {
+
+}
+uint8_t CPU6502::DEX() {
+	x -= 1;
+	setFlag(Z, x == 0x00);
+	setFlag(N, x & 0x80);
+	return 0;
+}
+uint8_t CPU6502::DEY() {
+	y -= 1;
+	setFlag(Z, y == 0x00);
+	setFlag(N, y & 0x80);
+	return 0;
+
+}
+uint8_t CPU6502::EOR() {
+   // -- a = a ^ memory
+      //occupy memory
+	fetch();
+	a = a ^ fetched;
+	   //set flags 
+	setFlag(Z, a == 0x00);
+	setFlag(N, a & 0x80);
+	return 0;
+}
+uint8_t CPU6502::CPX() {
+
+}
+uint8_t CPU6502::CPY() {
+
+}
+uint8_t CPU6502::LDX() {
+
+}
+uint8_t CPU6502::LDY() {
+
+}
+uint8_t CPU6502::LDA() {
+
+}
+uint8_t CPU6502::BIT() {
+
+}
+uint8_t CPU6502::BRK() {
+
+}
+uint8_t CPU6502::ASL() {
+
+}
+uint8_t CPU6502::TXA() {
+
+}
+uint8_t CPU6502::TYA() {
+
+}
+uint8_t CPU6502::CMP() {
+
+}
+uint8_t CPU6502::JSR() {
+
 }
